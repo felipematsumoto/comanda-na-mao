ecebcde36753400e7be29a1f3900955792413d13
Felipe <felipe@localhost.localdomain>
2018-08-27 19:10:07 -0400

    added settings to conect in the mysql database

--- comanda_na_mao/settings.py
+++ comanda_na_mao/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'comanda_na_mao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'comanda_na_mao',
+	'USER': 'root',
+	'PASSWORD': 'mypass',
+	'HOST': 'localhost',
+	'PORT': '3306',
     }
 }
 
