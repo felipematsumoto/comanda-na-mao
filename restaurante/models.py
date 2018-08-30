21545087de715abf5b6c7662660f2bb80ba748ac
FhKai <fhignacio@gmail.com>
2018-08-30 01:02:25 -0300

    Dados adicionados, pendencia em classe restaurante

--- restaurante/models.py
+++ restaurante/models.py
@@ -2,5 +2,23 @@ from django.db import models
 
 class Restaurante(models.Model):
     nome = models.CharField(max_length=50)
+    descricao = models.CharFiel(max_length=255)
+    endereco = models.CharField(max_length=155)
+    foto = models.FileField(upload_to='fotosRestaurante/')
+    tipoCozinha = models.CharField(max_length=50)
+    # SEMANA = (
+    #        ('Dom', 'Segunda'),
+    #        ('Seg', 'Segunda'),
+    #        ('Ter', 'Segunda'),
+    #        ('Qua', 'Segunda'),
+    #        ('Qui', 'Segunda'),
+    #        ('Sex', 'Segunda'),
+    #        ('Sab', 'Segunda'))
+
+
+    
+    nome = models.CharField(max_length=50)
     def __str__(self):
         return self.nome.encode('utf8','replace')
+
+
