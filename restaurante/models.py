e48f08c5039edd66dc38414173b6c357a5020b9d
FhKai <fhignacio@gmail.com>
2018-09-02 23:00:24 -0300

    classe Restaurante

--- restaurante/models.py
+++ restaurante/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 class Restaurante(models.Model):
     nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=255)
+    endereco = models.CharField(max_length=140)
+    cozinha = models.CharField(max_length=50)
     def __str__(self):
         return self.nome
