9bb610680a8866d730acf22650276694d213a447
FhKai <fhignacio@gmail.com>
2018-09-02 22:05:45 -0300

    Arrumar classe restaurante

--- restaurante/models.py
+++ restaurante/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Restaurante(models.Model):
     nome = models.CharField(max_length=50)
-    descricao = models.CharFiel(max_length=255)
+    descricao = models.CharField(max_length=255)
     endereco = models.CharField(max_length=155)
     foto = models.FileField(upload_to='fotosRestaurante/')
     tipoCozinha = models.CharField(max_length=50)
@@ -19,10 +19,5 @@ class Restaurante(models.Model):
     
     nome = models.CharField(max_length=50)
     def __str__(self):
-<<<<<<< HEAD
-        return self.nome.encode('utf8','replace')
-
-
-=======
         return self.nome
->>>>>>> 5e08017cb09c5f864a16eb0e9d25f6c71f419de4
+
